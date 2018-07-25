7f4da2bd1eb8666356c786487f0349ad7687d490
SnowSurfer <drinkaziu@gmail.com>
2018-07-25 14:49:52 -0400

    Draft Button logs selected player

--- src/components/PickNavBar.js
+++ src/components/PickNavBar.js
@@ -17,20 +17,20 @@ class PickNavBar extends React.Component {
   handleChange = (e, { searchQuery, value }) => this.setState({ searchQuery: '', value })
   handleSearchChange = (e, { searchQuery }) => this.setState({ searchQuery })
 
+  //Draft Button
+  handleButtonClick = (e) => console.log(e.target.value)
+
   render() {
     const { activeTab, searchQuery, value } = this.state
 
     const players = this.props.players.map(p => {
-      return {key: p.id, value: p.id, text: `${p.fname} ${p.lname} (${p.position}-${p.team})`}
+      return {key: p.id, value: p.displayName, text: `${p.fname} ${p.lname} (${p.position}-${p.team})`}
     })
 
     return(
       <Menu tabular>
         <Menu.Item name='Board' active={activeTab === 'Board'} onClick={this.handleTabClick} />
         <Menu.Item name='Rankings' active={activeTab === 'Rankings'} onClick={this.handleTabClick} />
-        <Menu.Item>
-          <Button color="google plus" >DRAFT</Button>
-        </Menu.Item>
         <Dropdown
           className="dropdown"
           compact
@@ -46,6 +46,14 @@ class PickNavBar extends React.Component {
           selection
           value={value}
         />
+        <Menu.Item>
+          <Button
+            color="google plus"
+            value={this.state.value}
+            onClick={this.handleButtonClick}
+            >DRAFT
+          </Button>
+        </Menu.Item>
       </Menu>
     )
   }
